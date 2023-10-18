5abfe8e51c0181193a6918fd790cf3d4b3ec3a4e
Upond <pond_chanawin@hotmail.com>
2023-10-18 17:11:56 +0700

    Update project
    Update that change using aria to date instead and update style in question answer

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const questionSection = document.querySelectorAll(".question-section")
 
 function toggleAnswer() {
-    const itemToggle = this.getAttribute('aria-expanded');
+    const itemToggle = this.getAttribute('data-expanded');
 
     for (i = 0; i < questionSection.length; i++) {
-        questionSection[i].setAttribute('aria-expanded', 'false')
+        questionSection[i].setAttribute('data-expanded', 'false')
     }
 
     if (itemToggle == 'false') {
-        this.setAttribute('aria-expanded', 'true');
+        this.setAttribute('data-expanded', 'true');
     }
 }
 
